8164bd7cf4d1dd53735729c3504bef4eecb15c9f
umitnrodemund <148437675+umitnrodemund@users.noreply.github.com>
2024-06-21 09:45:01 +0200

    update

---
+++ src/finalproject/p.py
@@ -0,0 +1,5 @@
+import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
